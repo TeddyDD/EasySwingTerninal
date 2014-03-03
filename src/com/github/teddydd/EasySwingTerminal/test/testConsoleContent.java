46f30dd8e7e52172e8476c93976b7d561f5abc9e
Daniel Lewan <vision360.daniel@gmail.com>
2014-03-03 20:39:24 +0100

    little more testing

--- src/com/github/teddydd/EasySwingTerminal/test/testConsoleContent.java
+++ src/com/github/teddydd/EasySwingTerminal/test/testConsoleContent.java
@@ -8,6 +8,7 @@ import java.awt.Color;
 import org.junit.Test;
 
 import com.github.teddydd.EasySwingTerminal.ConsoleContent;
+import com.github.teddydd.EasySwingTerminal.utils.Utils;
 
 public class testConsoleContent {
 
@@ -30,9 +31,29 @@ public class testConsoleContent {
 		assertNotNull("BG colors == null", cc.getBackgroundColorsArray());
 		
 		assertEquals("Problem with a size of the array",50,cc.getTextColorsArray().length);
-		
+		                          
 		assertEquals("filling the default value error / letters" ,' ', cc.getChar(1, 1));
 		assertEquals("filling the default value error / colorfg" ,Color.black, cc.getTextColor(1, 1));
+	}
+	
+	@Test
+	public void testGeneral() {
+		ConsoleContent cc = new ConsoleContent(5, 10);
+		
+		for (int y = 1; y < 5; y++) {
+			for (int x = 1; x < 10; x++) {
+				cc.setChar(x,y,(char)y);
+				assertEquals("char error set,get",(char)y, cc.getChar(x,y));
+				
+				Color randomColor = Utils.getRandomColor();
+				cc.setTextColor(x, y, randomColor);
+				cc.setBackgrounColor(x, y, randomColor);
+				
+				assertEquals("text color error",randomColor, cc.getTextColor(x, y));
+				assertEquals("bg color error",randomColor, cc.getBackgrounColor(x, y));
+			}
+		}
+		
 	}
 
 }
