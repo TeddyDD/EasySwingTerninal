f63d64431222aebc49d6e95a537c5cb26d09d544
Daniel Lewan <vision360.daniel@gmail.com>
2014-03-08 02:27:15 +0100

    working on writing colorfull chars api and testing

--- src/com/github/teddydd/EasySwingTerminal/HigLevelConsole.java
+++ src/com/github/teddydd/EasySwingTerminal/HigLevelConsole.java
@@ -3,23 +3,70 @@ package com.github.teddydd.EasySwingTerminal;
 import java.awt.Color;
 import java.awt.Font;
 
+import sun.java2d.loops.CustomComponent;
+
 public class HigLevelConsole extends Console{
 	
 	private ConsoleCursor cursor;
 	
-	private Color cursorColor;
+	private Color cursorColor, cursorTextColor;
+	private Color currentTextColor, currentBackgroundColor;
 	
 	public HigLevelConsole(int columns, int rows, Font font, Color defalutTextColor,
-			Color defalutBackgroundColor,Color cursorColor) {
+			Color defalutBackgroundColor,Color cursorColor,Color cursorTextColor) {
 		super(columns, rows, font, defalutTextColor, defalutBackgroundColor);
 		cursor = new ConsoleCursor(this);
 		this.cursorColor = cursorColor;
+		this.cursorTextColor = cursorTextColor;
+		this.currentTextColor = defalutTextColor;
+		this.currentBackgroundColor = defalutBackgroundColor;
 	}
 	
 	/** positions the cursor at the desired location - safe way */
 	public void setPostion(int column, int row) {
 		cursor.setPostion(column, row);
  	}
+	
+	public void nextCursorPostion() {
+		if (cursor.currentColumn == columns) {
+			cursor.setRow(cursor.currentRow++);
+			cursor.setColumn(1);
+		}
+		else {
+			cursor.currentColumn++;
+		}
+	}
+	
+	/**prints the character at the specified position using the currently selected color
+	 * @param column
+	 * @param row
+	 * @param c character to write
+	 */
+	public void writeChar(int column, int row, char c) {
+		setPostion(column, row);
+		buffer.setChar(cursor.currentColumn,cursor.currentRow,c);
+		buffer.setTextColor(cursor.currentColumn,cursor.currentRow, currentTextColor);
+		buffer.setBackgrounColor(cursor.currentColumn,cursor.currentRow, currentBackgroundColor);
+	}
+	
+	/**prints the character at the current position and moves the cursor on
+	 * @param c character to write
+	 */
+	public void writeChar(char c) {
+		writeChar(cursor.currentColumn, cursor.currentRow, c);
+		nextCursorPostion();
+	}
+	
+	/**Sets the colors used for writing characters
+	 * @param text Text color
+	 * @param background Background Color
+	 */
+	public void setColors(Color text, Color background) {
+		this.currentTextColor = text;
+		this.currentBackgroundColor = background;
+	}
+	
+	
 	
 	
 
---
+++ src/com/github/teddydd/EasySwingTerminal/test/AdvanceConsoleTest.java
@@ -0,0 +1,45 @@
+package com.github.teddydd.EasySwingTerminal.test;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+
+import com.github.teddydd.EasySwingTerminal.Console;
+import com.github.teddydd.EasySwingTerminal.HigLevelConsole;
+import com.github.teddydd.EasySwingTerminal.utils.DefaultConsoleSettings;
+
+public class AdvanceConsoleTest extends JFrame{
+	
+	private HigLevelConsole console;
+	private Font font;
+	
+	public AdvanceConsoleTest() {
+		setTitle("writing test");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800,600);
+		
+		font = DefaultConsoleSettings.font;
+		
+		console = new HigLevelConsole(10, 5, font, Color.black, Color.blue,null,null);
+		
+		getContentPane().add(console);
+
+		pack();
+		
+	}
+
+	public static void main(String[] args) {
+		AdvanceConsoleTest test = new AdvanceConsoleTest();
+		test.setVisible(true);
+		test.console.writeChar(1,1,'H');
+		test.console.writeChar(2,1,'#');
+		test.console.nextCursorPostion();
+		test.console.writeChar('3');
+		test.console.setColors(Color.red, Color.black);
+		test.console.writeChar('!');
+//		test.console.writeChar('e');
+		
+		
+	}
+}
